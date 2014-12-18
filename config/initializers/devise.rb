c6dc519519a955391c5c89e13d6a64131904c9a9
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-17 23:32:21 -0800

    sign in is working!

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -233,6 +233,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :bnet, ENV['BNET_KEY'], ENV['BNET_SECRET'], scope: "wow.profile,sc2.profile"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
