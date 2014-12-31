319451ab1c9382f84a64acbfa0e501432521fee9
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-30 17:41:58 -0800

    updated to limit results

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,10 @@ class Api::V1::UsersController < ApplicationController
   respond_to :json
 
   def search
-    @users = User.where("username LIKE ?", "%#{params[:q]}%")
-
-    respond_with @users
+    if params[:q]
+      @users = User.where("username LIKE ?", "%#{params[:q]}%").limit(10)
+    else
+      @users = User.all.limit(10)
+    end
   end
 end
