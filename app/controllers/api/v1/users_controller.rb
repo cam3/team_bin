1b2e0a2519230b3c1048584e95819d3f2677f919
Jason Barnett <jason.w.barnett@gmail.com>
2014-12-30 19:40:57 -0800

    made a little more flexible depending on js library

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,8 +2,9 @@ class Api::V1::UsersController < ApplicationController
   respond_to :json
 
   def search
-    if params[:q]
-      @users = User.where("username LIKE ?", "%#{params[:q]}%").limit(10)
+    query = params[:q] || params[:term] || nil
+    if query
+      @users = User.where("username LIKE ?", "%#{query}%").limit(10)
     else
       @users = User.all.limit(10)
     end
