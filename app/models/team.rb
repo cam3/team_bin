cde03cbba2cdcd61ceda91bff73e817ed6d93d33
Jason Barnett <jason.w.barnett@gmail.com>
2015-01-05 19:49:52 -0800

    downcase subdomain before save.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
+  before_save { |team| team.subdomain = team.subdomain.downcase }
   validates :subdomain, format: { with: /\A((?!-)[A-Za-z0-9-]{1,63}(?<!-))\z/, message: "Must only contain letters, numbers and dashes. Must NOT end or begin with a dash." }
 
   has_many :users, through: :memberships
