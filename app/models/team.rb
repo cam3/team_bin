607ee0e7e7ac2548147274e53c7d8d6e8a5dfd95
Jason Barnett <jason.w.barnett@gmail.com>
2015-01-05 18:51:12 -0800

    add subdomain validation for team

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ActiveRecord::Base
+  validates :subdomain, format: { with: /\A((?!-)[A-Za-z0-9-]{1,63}(?<!-))\z/, message: "Must only contain letters, numbers and dashes. Must NOT end or begin with a dash." }
+
   has_many :users, through: :memberships
   has_many :memberships
 end
